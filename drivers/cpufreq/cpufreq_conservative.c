383b83d0dd3f0250a6b0559c4b89a12dfa6418ef
faux123 <reioux@gmail.com>
2012-02-02 09:42:36 -0800

    cpufreq: update early suspend level to EARLY_SUSPEND_LEVEL_STOP_DRAWING
    conservative, ondemand and interactive governors, update their early suspend
    level from EARLY_SUSPEND_LEVEL_BLANK_SCREEN to
    EARLY_SUSPEND_LEVEL_STOP_DRAWING to avoid race conditions with rapid screen
    on/off via gpio
    (cherry picked from commit fe6ff53c4e08fa8c489e1d71e8693d4a135a443b)
    
    Conflicts:
    
    	drivers/cpufreq/cpufreq_conservative.c
    	drivers/cpufreq/cpufreq_ondemand.c

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -602,6 +602,17 @@ struct cpufreq_governor cpufreq_gov_conservative = {
 
 static int __init cpufreq_gov_dbs_init(void)
 {
+
+#ifdef CONFIG_HAS_EARLYSUSPEND
+	cpufreq_gov_lcd_status = 1;
+
+	cpufreq_gov_early_suspend.level = EARLY_SUSPEND_LEVEL_STOP_DRAWING + 1;
+
+	cpufreq_gov_early_suspend.suspend = cpufreq_gov_suspend;
+	cpufreq_gov_early_suspend.resume = cpufreq_gov_resume;
+	register_early_suspend(&cpufreq_gov_early_suspend);
+#endif
+
 	return cpufreq_register_governor(&cpufreq_gov_conservative);
 }
 
